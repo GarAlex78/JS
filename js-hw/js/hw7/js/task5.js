dc9a8ab203def34a943e40c535388d7fcdcd264a
GarAlex78 <57135333+GarAlex78@users.noreply.github.com>
2020-03-22 12:32:36 +0200

    add hw 7_5

--- js-hw/js/hw7/js/task5.js
+++ js-hw/js/hw7/js/task5.js
@@ -1,1 +1,15 @@
 "use strict";
+/*
+Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+<input type="text" placeholder="Ваше имя?" id="name-input" />
+<h1>Привет, <span id="name-output">незнакомец</span>!</h1>
+*/
+//Event.preventDefault()- запрещяем браузеру выполнять действия по умолчанию
+//debugger;
+const inputText = document.querySelector("#name-input");
+const outputText = document.querySelector("#name-output");
+inputText.addEventListener("input", addTextInSpan);
+function addTextInSpan(e) {
+  outputText.textContent =
+    e.currentTarget.value.length !== 0 ? e.currentTarget.value : "незнакомец";
+}
